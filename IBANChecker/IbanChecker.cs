be10f83bdce25f228e23020ac5329e9c5e8d4353
Burhan SAFRAN <burhan.safran@st.uskudar.edu.tr>
2022-10-29 01:29:14 +0300

    Proje dosyası ekle.

---
+++ IBANChecker/IbanChecker.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Linq;
+using System.Numerics;
+
+namespace IBANChecker
+{
+    public class IbanChecker
+    {
+        private string iban;
+
+        /// <summary>
+        /// The International Bank Account Number (IBAN) Checker with Modulo 97 Algorithm 
+        /// </summary>
+        /// <param name="iban"></param>
+        public IbanChecker(string iban)
+        {
+            this.iban = iban.Trim().Replace(" ", "");
+        }
+
+        public bool IsValid()
+        {
+            iban = iban.Substring(4) + iban.Substring(0, 4); // Move first four to the end
+            string number = string.Join(string.Empty, iban.Select(x => char.IsDigit(x) ? char.GetNumericValue(x).ToString(CultureInfo.InvariantCulture) : (x - 55).ToString())); // Replace characters with digits
+            return BigInteger.Parse(number) % 97 == 1; // Calculate d = N mod 97 If d == 1 then valid, otherwise invalid.
+        }
+    }
+}
